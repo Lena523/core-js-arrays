4dca99c2613624808b79c29fd1f0787a44b5e58e
Elena Khadasevich <camatsu90@mail.ru>
2024-12-22 05:47:48 +0300

    feat: add new task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -463,8 +463,16 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array(n)
+    .fill(0)
+    .map(function (elem, i) {
+      return Array(n)
+        .fill(0)
+        .map(function (elem2, j) {
+          return 1 - Math.min(Math.abs(i - j), 1);
+        });
+    });
 }
 
 /**
